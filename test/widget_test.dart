c9989824389ec6d019dfeacf08668a6fe0f03ced
wfaulk5 <williamfaulk04@gmail.com>
2026-09-17 16:12:25 -0500

    adding tests

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -24,6 +24,42 @@ void main() {
     expect(display(tester), '4');
   });
 
+  testWidgets('3 times 5', (tester) async {
+    // setup
+    await tester.pumpWidget(const MyApp());
+    // select 2, +, 2, =
+    await press(tester, ['3', '×', '5', '=']);
+    // compare against expected
+    expect(display(tester), '15');
+  });
+
+  testWidgets('9 divided by 3', (tester) async {
+    // setup
+    await tester.pumpWidget(const MyApp());
+    // select 2, +, 2, =
+    await press(tester, ['9', '÷', '3', '=']);
+    // compare against expected
+    expect(display(tester), '3');
+  });
+
+  testWidgets('-4 plus -2', (tester) async {
+    // setup
+    await tester.pumpWidget(const MyApp());
+    // select 2, +, 2, =
+    await press(tester, ['4', '±', '+', '2', '±', '=']);
+    // compare against expected
+    expect(display(tester), '-6');
+  });
+
+  testWidgets('9 times 3', (tester) async {
+    // setup
+    await tester.pumpWidget(const MyApp());
+    // select 2, +, 2, =
+    await press(tester, ['9', '×', '3', '=']);
+    // compare against expected
+    expect(display(tester), '27');
+  });
+
   testWidgets('Basic arithmetic and left-to-right chaining', (tester) async {
     await tester.pumpWidget(const MyApp());
     await press(tester, ['8', '+', '2', '=']);
